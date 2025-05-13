dca04073f6b3a40123e54338889c4deb14a376b7
wxg115 <74805198+wxg115@users.noreply.github.com>
2025-05-13 09:11:54 +0300

    Create timerbot.py

---
+++ week11/timerbot.py
@@ -0,0 +1,17 @@
+##EDTING CODE
+def takePhoto():
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    if not cap.isOpend():
+        print("camera open error")
+        return
+    ret, image=cap.read()
+    if not ret:
+        print("frame read error")
+        return
+    cv2.imshow("CAMERA", image)
+    time.sleep(1)
+    cv2.imwrite("./image.jpg", image)
+    cap.release()
+    cv2.destroyAllWindows()
