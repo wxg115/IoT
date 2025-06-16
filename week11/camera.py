52f2dc4d648f2846a2d7f51b56fe683195a84d3a
wxg115 <74805198+wxg115@users.noreply.github.com>
2025-06-16 22:23:48 +0900

    Update camera.py

--- week11/camera.py
+++ week11/camera.py
@@ -5,7 +5,7 @@ import time
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-if not cap.isOpned():
+if not cap.isOpened():
     print("camera open error")
     exit()
 
