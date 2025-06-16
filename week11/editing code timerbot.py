c9d481057baf367b488e8d9d6bf91a7532f83f1d
wxg115 <74805198+wxg115@users.noreply.github.com>
2025-06-16 22:22:28 +0900

    Update editing code timerbot.py

--- week11/editing code timerbot.py
+++ week11/editing code timerbot.py
@@ -4,7 +4,7 @@ def takePhoto():
     cap = cv2.VideoCapture(0)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
-    if not cap.isOpend():
+    if not cap.isOpened():
         print("camera open error")
         return
     ret, image=cap.read()
